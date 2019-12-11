f0199261646ed7a62eb66773a99b14bf7f4950d8
dilshan97 <dilshanramesh81@gmail.com>
2019-12-11 20:17:43 +0530

    update all countries.dart file

---
+++ lib/Screens/AllCountries.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class AllContries extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.cyan,
+        title: Text('All Countries'),
+      ),
+      body: Container(
+        child: Text('Hello'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'Screens/AllCountries.dart';
+
 void main() {
   runApp(new MaterialApp(
     home: AllContries(),
   ));
 }
 
-class AllContries extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.cyan,
-        title: Text('All Countries'),
-      ),
-    );
-  }
-}
+
