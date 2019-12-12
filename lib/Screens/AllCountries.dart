c0d3eab5dfe9afd3bd678f014b20a27d7439fd84
dilshan97 <dilshanramesh81@gmail.com>
2019-12-12 13:23:20 +0530

    data fetch and initiated with list

--- lib/Screens/AllCountries.dart
+++ lib/Screens/AllCountries.dart
@@ -4,21 +4,26 @@ import 'package:flutter/material.dart';
 import 'Country.dart';
 
 class AllContries extends StatefulWidget {
-
   @override
   _AllContriesState createState() => _AllContriesState();
 }
 
 class _AllContriesState extends State<AllContries> {
+  Future<List> countries;
 
-  void getCountries() async {
+  Future<List> getCountries() async {
     var response = await Dio().get('https://restcountries.eu/rest/v2/all');
-    print(response.data);
+    return response.data;
+  }
+
+  @override
+  void initState() {
+    countries = getCountries();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-
     getCountries();
 
     // TODO: implement build
@@ -29,30 +34,37 @@ class _AllContriesState extends State<AllContries> {
       ),
       body: Container(
           padding: EdgeInsets.all(10),
-          child: ListView(
-            children: <Widget>[
-              GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => Country('Sri Lanaka'),
+          child: FutureBuilder<List>(
+            future: countries,
+            builder: (BuildContext context, AsyncSnapshot<List> snapshot){
+
+              if(snapshot.hasData){
+                return ListView.builder(itemBuilder: (BuildContext context, int index){
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => Country(snapshot.data[index]['name']),
+                        ),
+                      );
+                    },
+                    child: Card(
+                      elevation: 10,
+                      color: Colors.blueGrey,
+                      child: Padding(
+                        padding:
+                        const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                        child: Text(
+                          snapshot.data[index]['name'],
+                          style: TextStyle(fontSize: 10),
+                        ),
+                      ),
                     ),
                   );
-                },
-                child: Card(
-                  elevation: 10,
-                  color: Colors.blueGrey,
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                    child: Text(
-                      'Sri Lanaka',
-                      style: TextStyle(fontSize: 10),
-                    ),
-                  ),
-                ),
-              )
-            ],
+                });
+              }
+              return null;
+            },
           )),
     );
   }
