257d8743b491f6c29dcdcdf2e0d095250d342789
dilshan97 <dilshanramesh81@gmail.com>
2019-12-12 15:45:15 +0530

    remove future builder and add progress indicator

--- lib/Screens/AllCountries.dart
+++ lib/Screens/AllCountries.dart
@@ -11,18 +11,24 @@ class AllContries extends StatefulWidget {
 class _AllContriesState extends State<AllContries> {
   bool isSearching = false;
 
-  Future<List> countries;
+  List countries = [];
 
-  Future<List> getCountries() async {
+  getCountries() async {
     var response = await Dio().get('https://restcountries.eu/rest/v2/all');
     return response.data;
   }
 
   @override
   void initState() {
-    countries = getCountries();
+    getCountries().then((data) {
+      setState(() {
+        countries = data;
+      });
+    });
     super.initState();
   }
+
+  void _filterCountry(value) {}
 
   @override
   Widget build(BuildContext context) {
@@ -37,12 +43,16 @@ class _AllContriesState extends State<AllContries> {
             : TextField(
                 style: TextStyle(color: Colors.white),
                 decoration: InputDecoration(
-                    icon: Icon(
-                      Icons.search,
-                      color: Colors.white,
-                    ),
-                    hintStyle: TextStyle(color: Colors.white),
-                    hintText: 'Search Country Here'),
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                  hintStyle: TextStyle(color: Colors.white),
+                  hintText: 'Search Country Here',
+                ),
+                onChanged: (value) {
+                  _filterCountry(value);
+                },
               ),
         actions: <Widget>[
           isSearching
@@ -66,38 +76,35 @@ class _AllContriesState extends State<AllContries> {
       ),
       body: Container(
           padding: EdgeInsets.all(10),
-          child: FutureBuilder<List>(
-            future: countries,
-            builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                    itemBuilder: (BuildContext context, int index) {
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => Country(snapshot.data[index]),
-                        ),
-                      );
-                    },
-                    child: Card(
-                      elevation: 10,
-                      color: Colors.blueGrey,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 8),
-                        child: Text(
-                          snapshot.data[index]['name'],
-                          style: TextStyle(fontSize: 10),
+          child: countries.length > 0
+              ? ListView.builder(
+                  itemCount: countries.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => Country(countries[index]),
+                          ),
+                        );
+                      },
+                      child: Card(
+                        elevation: 10,
+                        color: Colors.blueGrey,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 8),
+                          child: Text(
+                            countries[index]['name'],
+                            style: TextStyle(fontSize: 10),
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                });
-              }
-              return null;
-            },
-          )),
+                    );
+                  })
+              : Center(
+                  child: CircularProgressIndicator(),
+                )),
     );
   }
 }
